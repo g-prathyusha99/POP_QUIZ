55d0e2d37863524d2b76c5a40e0745fb7a46c4fb
Prathyusha G <34506767+g-prathyusha99@users.noreply.github.com>
2023-10-28 22:55:33 -0400

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,90 @@
+var total=10;
+var question=new Array();
+for (i=1;i<=total+1;i++){
+temp="choice"+i+"=new Array()";
+eval(temp);
+}
+var solution=new Array();
+
+/*Below lists the phrases that will be randomly displayed if the user correctly answers the question. You may extend or shorten this list as desired*/
+var compliments=new Array();
+compliments[0]="Excellent!";
+compliments[1]="Wow, you're really rocking!";
+compliments[2]="You must have studied hard. Good job!";
+compliments[3]="Right on.";
+compliments[4]="Correct!";
+compliments[5]="Great Job!";
+compliments[6]="Good work!";
+
+/*Below lists the questions, its choices, and finally, the solution to each question. Follow the exact format below when editing the questions. You may have as many questions as needed.*/
+
+question[1]="👨🏻‍🔬🦠"
+choice1[1]="Inception"
+choice1[2]="Madagascar"
+choice1[3]="Ant-man"
+choice1[4]="Flubber"
+
+question[2]="🧙‍♂️🏰🚂"
+choice2[1]="Hunger Games"
+choice2[2]="Harry Potter"
+choice2[3]="The Polar Express"
+choice2[4]="Twilight"
+
+question[3]="👽🚲🌕"
+choice3[1]="Star Wars"
+choice3[2]="Mars Attacks!"
+choice3[3]="E.T."
+choice3[4]="Close Encounters of the Third Kind"
+
+question[4]="🍫🏭"
+choice4[1]="Candyman"
+choice4[2]="The Chocolate Soldier"
+choice4[3]="The Golden Compass"
+choice4[4]="Charlie and the Chocolate Factory"
+
+question[5]="🦁👑"
+choice5[1]="Lionheart"
+choice5[2]="The Chronicles of Narnia"
+choice5[3]="The Lion King"
+choice5[4]="Jungle Book"
+
+question[6]="🕷️🤟🏙️"
+choice6[1]="Ant-man"
+choice6[2]="Spider-man"
+choice6[3]="Bug's Life"
+choice6[4]="The Fly"
+
+question[7]="🦖🦕🏞️"
+choice7[1]="A Night at the Museum"
+choice7[2]="King Kong"
+choice7[3]="Land Before Time"
+choice7[4]="Jurassic Park"
+
+question[8]="🔍🐠"
+choice8[1]="Free Willy"
+choice8[2]="Finding Nemo"
+choice8[3]="Big Fish"
+choice8[4]="Shark Tale"
+
+question[9]="❄️👸"
+choice9[1]="Tangled"
+choice9[2]="Frozen"
+choice9[3]="Ice Age"
+choice9[4]="Brave"
+
+question[10]="🌪️👠🐶"
+choice10[1]="Cinderella"
+choice10[2]="Peter Pan"
+choice10[3]="The Wizard of Oz"
+choice10[4]="101 Dalmatians"
+
+solution[1]="d";
+solution[2]="b";
+solution[3]="c";
+solution[4]="d";
+solution[5]="c";
+solution[6]="b";
+solution[7]="d";
+solution[8]="b";
+solution[9]="b";
+solution[10]="c";
